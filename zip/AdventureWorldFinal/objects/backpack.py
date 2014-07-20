19fe6be63ff3851cc0b235dae27d752292158cf7
Tanya Gray <tanyagray.nz@gmail.com>
2014-07-20 16:03:10 +1200

    Added completed game code
    Signed-off-by: Tanya Gray <tanyagray.nz@gmail.com>

---
+++ zip/AdventureWorldFinal/objects/backpack.py
@@ -0,0 +1,17 @@
+class Backpack(object):
+
+    def __init__(self):
+            self.items = []
+
+
+    def getItem(self, wantedItemName):
+        
+        wantedItem = None
+        
+        for item in self.items:
+            if item.name == wantedItemName:
+                wantedItem = item
+        
+        return wantedItem
+        
+    
---
+++ zip/AdventureWorldFinal/rooms/alleyway.py
@@ -0,0 +1,24 @@
+from rooms.room import Room
+from objects.door import Door
+
+class Alleyway(Room):
+    
+    def __init__(self):
+        
+        # the name of the room
+        self.name = "The Alleyway"
+        
+        # a description of the room to show the user
+        self.description = "The alleyway is dirty and slimy. You get a chill just standing here. There is a red door on the left side of the alley, a green door on the right, and a black spiked gate at one end."
+        
+        # create all the doors leading out of the room
+        self.doors = [
+            Door('r', 'Red Door', 'Kitchen'),
+            Door('g', 'Green Door', 'Dining Hall'),
+            Door('b', 'Black Gate', 'Gardens')
+        ]
+        
+        # create all the items you can interact with in the room
+        self.items = []
+        
+    
