cb0e00b780853d8531f3588c8675d4852c8ca67c
Tanya Gray <tanyagray.nz@gmail.com>
2014-07-20 16:03:51 +1200

    site updates
    Signed-off-by: Tanya Gray <tanyagray.nz@gmail.com>

---
+++ _site/zip/AdventureWorldFinal/objects/door.py
@@ -0,0 +1,7 @@
+class Door(object):
+
+	def __init__(self, button, name, leadsTo, requiredItem = None):
+		self.button = button
+		self.name = name
+		self.leadsTo = leadsTo
+		self.requiredItem = requiredItem
